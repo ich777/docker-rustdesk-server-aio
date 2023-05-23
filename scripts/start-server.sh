ba7d3346eeaae8bbaf4fc135e6691fcda09e2eeb
ich777 <christophhummer@gmail.com>
2023-05-23 14:58:42 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-sleep infinity
+if [ "${HBBS_ENABLED}" != "true" ] && [ "${HBBR_ENABLED}" != "true" ]; then
+  echo "---hbbs and hbbr disabled, please enable at least one, putting container into sleep mode!---"
+  sleep infinity
+fi
+
+echo "---Starting RustDesk-Server-AiO---"
+if [ "${HBBS_ENABLED}" == "true" ]; then
+  echo "---Starting hbbs---"
+  /usr/bin/hbbs ${HBBS_PARAMS} &
+else
+  echo "---hbbs disabled!---"
+fi
+
+if [ "${HBBR_ENABLED}" == "true" ]; then
+  echo "---Starting hbbr---"
+  /usr/bin/hbbr ${HBBR_PARAMS} &
+else
+  echo "---hbbr disabled!---"
+fi
+
+/opt/scripts/start-watchdog.sh

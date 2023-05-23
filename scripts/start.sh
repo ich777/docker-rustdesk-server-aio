44867bba1da754f3beea161e6f6121c0b9cd0cbb
ich777 <christophhummer@gmail.com>
2023-05-23 15:31:38 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,8 +26,10 @@ chown -R ${UID}:${GID} ${DATA_DIR}
 
 echo "---Starting...---"
 term_handler() {
-	kill -SIGTERM "$killpid"
-	wait "$killpid" -f 2>/dev/null
+	termpid=$(pidof hbbs hbbr)
+	kill -SIGINT ${termpid}
+	tail --pid=${termpid// / --pid=} -f 2>/dev/null
+	sleep 0.5
 	exit 143;
 }
 
